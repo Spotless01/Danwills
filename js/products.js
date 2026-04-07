b267437d123bf5a93c2fb961f57470b3ff1e4b42
Spotless01 <emmanuelmieza01@gmail.com>
2026-04-07 15:00:05 +0000

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,99 +1,164 @@
 const products=[
 {
   id:1,
-  name:"Creed Fragrance",
-  price:350,
-  category:"Women",
-  image:"https://m.media-amazon.com/images/I/61iYswLBuXL.jpg",
-  rating: 5,
+  name:"Aventos",
+  price: 50,
+  category:"Unisex",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/69/8694011/1.jpg?2458",
+  rating: 4,
   accords: ["Woody","Amber","Smoky"],
-  longevity: 85,
-  projection: 75,
+  longevity: 60,
+  projection: 60,
   notes: {
-            top: ["Bergamot","Lemon"],
-            middle: ["Rose","Jasmine"],
-            base: ["Oud","Amber","Musk"]
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
          }
 },
 {
   id:2,
-  name:" Tom Ford Private Blend",
-  price:350,
-  category:"Women",
-  image:"https://www.tomfordbeauty.com/cdn/shop/files/tf_sku_T1XF01_2000x2000_3.jpg?v=1774390824&width=2000",
+  name:"Brown Orchid Eau de Parfum Spray - 80ml",
+  price: 82,
+  category:"Unisex",
+  image:"https://gh.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/84/829813/1.jpg?8110",
   rating: 4,
   accords: ["Woody","Amber","Smoky"],
-  longevity: 85,
-  projection: 75,
+  longevity: 60,
+  projection: 60,
   notes: {
-            top: ["Bergamot","Lemon"],
-            middle: ["Rose","Jasmine"],
-            base: ["Oud","Amber","Musk"]
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
          }
 },
 {
   id:3,
-  name:" Clive Christia",
-  price:350,
-  category:"Women",
-  image:"https://cdn.fragrancenet.com/images/photos/1600x1600/471161.jpg",
+  name:"Musamam Eau De Farfum- 100ml",
+  price: 520,
+  category:"Unisex",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/40/7950982/2.jpg?5943",
   rating: 4,
   accords: ["Woody","Amber","Smoky"],
-  longevity: 85,
-  projection: 75,
+  longevity: 80,
+  projection: 95,
   notes: {
-            top: ["Bergamot","Lemon"],
-            middle: ["Rose","Jasmine"],
-            base: ["Oud","Amber","Musk"]
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
          }
 },
 {
   id:4,
-  name:" Roja Parfums",
-  price:350,
-  category:"Women",
-  image:"https://amarisbeauty.com/cdn/shop/files/ELSIUM-NOIRNEW_1000x.png?v=1761884989",
+  name:"Tobacco Rouge Perfume 100ml",
+  price: 250,
+  category:"Male",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/45/4006261/1.jpg?8889",
   rating: 4,
   accords: ["Woody","Amber","Smoky"],
-  longevity: 85,
-  projection: 75,
+  longevity: 55,
+  projection: 62,
   notes: {
-            top: ["Bergamot","Lemon"],
-            middle: ["Rose","Jasmine"],
-            base: ["Oud","Amber","Musk"]
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
          }
 },
 {
   id:5,
-  name:" Byredo",
-  price:350,
-  category:"Women",
-  image:"https://skinlife.pt/wp-content/uploads/Byredo-Vanille-Antique-Extrait-de-Parfum-50ml-2-1.jpg",
+  name:"Golden Night Body Spray - 200ml",
+  price: 70,
+  category:"Female",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/14/657094/1.jpg?5927",
   rating: 4,
   accords: ["Woody","Amber","Smoky"],
-  longevity: 85,
-  projection: 75,
+  longevity: 42,
+  projection: 56,
   notes: {
-            top: ["Bergamot","Lemon"],
-            middle: ["Rose","Jasmine"],
-            base: ["Oud","Amber","Musk"]
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
          }
 },
 {
   id:6,
-  name:" Maison Francis Kurkdjian",
-  price:350,
-  category:"Women",
-  image:"https://i5.walmartimages.com/seo/Maison-Francis-Kurkdjian-Baccarat-Rouge-540-Pure-Perfume-2-4-Fl-Oz_ec2511a1-fc53-41e1-b763-f1fa63952abd.7a66519b8530c93f2e8d6713dadc210b.jpeg",
+  name:"SWEET NIGHT Wild Men's Fragrance-50ml",
+  price: 70,
+  category:"Male",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/08/1505003/2.jpg?9843",
   rating: 4,
   accords: ["Woody","Amber","Smoky"],
-  longevity: 85,
-  projection: 75,
+  longevity: 26,
+  projection: 50,
   notes: {
-            top: ["Bergamot","Lemon"],
-            middle: ["Rose","Jasmine"],
-            base: ["Oud","Amber","Musk"]
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
          }
-}
+},
+{
+  id:7,
+  name:"SWEET NIGHT Colorful Perfume - 65ml",
+  price: 70,
+  category:"Male",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/51/2705003/1.jpg?9410",
+  rating: 5,
+  accords: ["Woody","Amber","Smoky"],
+  longevity: 32,
+  projection: 44,
+  notes: {
+            top: ["",""],
+            middle: ["",""],
+            base: ["","",""]
+         }
+},
+{
+  id:8,
+  name:"Strawberry Vanilla Fine Fragrance - 236ml",
+  price: 60,
+  category:"Female",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/54/9205003/1.jpg?4663",
+  rating: 4,
+  accords: ["Woody","Amber","Smoky"],
+  longevity: 32,
+  projection: 44,
+  notes: {
+            top: ["Aventos"],
+            middle: ["Golden Night Body Spray"],
+            base: ["SWEET NIGHT Colorful Perfume"]
+         }
+},
+{
+  id:9,
+  name:"Portable Refillable Spray Perfume Bottle Cosmetic Container - 5ML Gold",
+  price: 100,
+  category:"Unisex",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/12/3032482/1.jpg?3607",
+  rating: 5,
+  accords: ["Woody","Amber","Smoky"],
+  longevity: 80,
+  projection: 95,
+  notes: {
+            top: ["Aventos"],
+            middle: ["Golden Night Body Spray"],
+            base: ["SWEET NIGHT Colorful Perfume"]
+         }
+},
+{
+  id:10,
+  name:"Mr England Touch for Men Eau de Parfum 100ml",
+  price: 250,
+  category:"Men",
+  image:"https://gh.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/82/7324451/1.jpg?8805",
+  rating: 4,
+  accords: ["Woody","Amber","Smoky"],
+  longevity: 86,
+  projection: 95,
+  notes: {
+            top: ["Strawberry Vanilla Fine Fragrance"],
+            middle: ["Golden Night Body Spray"],
+            base: ["SWEET NIGHT Colorful Perfume"]
+         }
+},
+
 
 ];
