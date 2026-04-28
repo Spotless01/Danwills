e2c0c9b21256edb8b2e3d30e6de6744aed2ff004
Spotless01 <emmanuelmieza01@gmail.com>
2026-04-28 14:17:39 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,19 @@
+import { initializeApp, getApps, getApp } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
 
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+
+// YOUR CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyCVLoGB4S25-HSHx_HCtltGs1u_-U4n9lE",
+  authDomain: "danwilhs-store.firebaseapp.com",
+  projectId: "danwilhs-store",
+};
+
+// ✅ PREVENT DUPLICATE INITIALIZATION
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
+
+export const db = getFirestore(app);
